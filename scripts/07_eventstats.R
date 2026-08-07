75fb63363084656f635bf5060e83322dc3a62c79
AJW <alx.webster@gmail.com>
2026-08-07 16:29:10 -0600

    condensed event stats calculation

--- scripts/07_eventstats.R
+++ scripts/07_eventstats.R
@@ -47,10 +47,10 @@ gwvar_well<-c(cv(EXOz.dtw[["SLOC"]]$DTW_m),
 gwmv_well<-data.frame(wells,gwmean_well,gwvar_well,gwtruevar_well)
 gwmv_well
 
-#### export for use in other scripts ####
+#### Export for use in other scripts ####
 write.csv(gwmv_well, "DTW_compiled/gwmv_well.csv")
 
-#### import list of event dates per well ####
+#### Import list of event dates per well ####
 BEGI_events = readRDS("EXO_compiled/BEGI_events.rds")
 
 #Turns lists into vectors
@@ -60,174 +60,42 @@ VDOS_dates <- c(BEGI_events[["Eventdate"]][["VDOS_dates"]])
 VDOW_dates <- c(BEGI_events[["Eventdate"]][["VDOW_dates"]])
 
 
-#### DO event dtw mean (2 days) ####
-#SLOC
-#mean calculated 2 days before each event
-SLOC_event_mean<-numeric()
+#### DO event dtw mean, variance, SD, and CV (2 days before each event) ####
 
-for (i in c(1:length(BEGI_events[["DO_events"]][["SLOC_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  SLOC_mean <- EXOz.dtw[["SLOC"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(SLOC_mean = mean(DTW_m, na.rm = TRUE))
-  #mean of DTW_m over period of temptimes
-  #add mean to SLOC_event_mean
-  SLOC_event_mean <- c(SLOC_event_mean,SLOC_mean)
+wells <- c("SLOC", "SLOW", "VDOS", "VDOW")
+
+dtw_event_stats_list <- list()
+
+for (site in wells) {
+  site_key <- paste0(site, "_DO")
+  site_events <- BEGI_events[["DO_events"]][[site_key]]
+  
+  for (event_name in names(site_events)) {
+    event_time <- site_events[[event_name]]$datetimeMT[1]
+    start_time <- event_time - (60 * 60 * 48)
+    
+    window_vals <- EXOz.dtw[[site]] %>%
+      filter(datetimeMT >= start_time & datetimeMT < event_time) %>%
+      pull(DTW_m)
+    
+    event_var <- var(window_vals, na.rm = TRUE)
+    
+    dtw_event_stats_list[[length(dtw_event_stats_list) + 1]] <- data.frame(
+      site = site,
+      event_id = event_name,
+      Eventdate = event_time,
+      DTW_mean_2d = mean(window_vals, na.rm = TRUE),
+      DTW_var_2d = event_var,
+      DTW_sd_2d = sd(window_vals, na.rm = TRUE),
+      DTW_cv_2d = cv(window_vals),
+      DTW_relvar_2d = event_var / gwtruevar_well[[site]] # relative variance: this event's 2-day variance as a multiple of that well's own overall variance (>1 = more variable than usual for this well; <1 = calmer than usual for this well). 
+    )
+  }
 }
 
-SLOC_event_mean = unlist(SLOC_event_mean,use.names = F)
+DO_event_mv <- bind_rows(dtw_event_stats_list)
 
-#SLOW
-#mean calculated 2 days before each event
-SLOW_event_mean<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["SLOW_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  SLOW_mean <- EXOz.dtw[["SLOW"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(SLOW_mean = mean(DTW_m, na.rm = TRUE))
-  #mean of DTW_m over period of temptimes
-  #add mean to SLOW_event_mean
-  SLOW_event_mean <- c(SLOW_event_mean,SLOW_mean)
-}
-
-SLOW_event_mean = unlist(SLOW_event_mean,use.names = F)
-
-
-#VDOS
-#mean calculated 2 days before each event
-VDOS_event_mean<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["VDOS_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  VDOS_mean <- EXOz.dtw[["VDOS"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(VDOS_mean = mean(DTW_m, na.rm = TRUE))
-  #mean of DTW_m over period of temptimes
-  #add mean to VDOS_event_mean
-  VDOS_event_mean <- c(VDOS_event_mean,VDOS_mean)
-}
-
-VDOS_event_mean = unlist(VDOS_event_mean,use.names = F)
-
-
-#VDOW
-#mean calculated 2 days before each event
-VDOW_event_mean<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["VDOW_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  VDOW_mean <- EXOz.dtw[["VDOW"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(VDOW_mean = mean(DTW_m, na.rm = TRUE))
-  #mean of DTW_m over period of temptimes
-  #add mean to VDOW_event_mean
-  VDOW_event_mean <- c(VDOW_event_mean,VDOW_mean)
-}
-
-VDOW_event_mean = unlist(VDOW_event_mean,use.names = F)
-
-
-#### DO event dtw CV (2 days) ####
-# note from AJW: I need the events labeled by date (or something more sophisticated if they span multiple dates) so that I can match them to the AUC etc. results for modeling!
-
-#SLOC
-#CV calculated 2 days before each event
-SLOC_event_cv<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["SLOC_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  SLOC_cv <- EXOz.dtw[["SLOC"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(SLOC_cv = cv(DTW_m))
-  #cv of DTW_m over period of temptimes
-  #add cv to SLOC_event_cv
-  SLOC_event_cv <- c(SLOC_event_cv,SLOC_cv)
-}
-
-SLOC_event_cv = unlist(SLOC_event_cv,use.names = F)
-
-
-#SLOW
-#CV calculated 2 days before each event
-SLOW_event_cv<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["SLOW_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  SLOW_cv <- EXOz.dtw[["SLOW"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(SLOW_cv = cv(DTW_m))
-  #cv of DTW_m over period of temptimes
-  #add cv to SLOW_event_mean
-  SLOW_event_cv <- c(SLOW_event_cv,SLOW_cv)
-}
-
-SLOW_event_cv = unlist(SLOW_event_cv,use.names = F)
-
-
-#VDOS
-#CV calculated 2 days before each event
-VDOS_event_cv<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["VDOS_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  VDOS_cv <- EXOz.dtw[["VDOS"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(VDOS_cv = cv(DTW_m))
-  #cv of DTW_m over period of temptimes
-  #add cv to VDOS_event_mean
-  VDOS_event_cv <- c(VDOS_event_cv,VDOS_cv)
-}
-
-VDOS_event_cv = unlist(VDOS_event_cv,use.names = F)
-
-
-#VDOW
-#CV calculated 2 days before each event
-VDOW_event_cv<-numeric()
-
-for (i in c(1:length(BEGI_events[["DO_events"]][["VDOW_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1], 
-                  by = '15 mins')
-  VDOW_cv <- EXOz.dtw[["VDOW"]] %>%
-    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
-    summarise(VDOW_cv = cv(DTW_m))
-  #cv of DTW_m over period of temptimes
-  #add cv to VDOW_event_mean
-  VDOW_event_cv <- c(VDOW_event_cv,VDOW_cv)
-}
-
-VDOW_event_cv = unlist(VDOW_event_cv,use.names = F)
-
-
-#### Dataframe of event mean/var for 2 days ####
-#BEGI_events[["Eventdate"]][["SLOC_DO"]]
-DO_event_mean <- c(SLOC_event_mean,SLOW_event_mean,VDOS_event_mean,VDOW_event_mean)
-DO_event_cv <- c(SLOC_event_cv,SLOW_event_cv,VDOS_event_cv,VDOW_event_cv)
-Eventdates<-c(SLOC_dates,SLOW_dates,VDOS_dates,VDOW_dates)
-WellID<-c(rep(c("SLOC","SLOW","VDOS","VDOW"),
-              times=c(length(SLOC_event_mean),length(SLOW_event_mean),length(VDOS_event_mean),length(VDOW_event_mean))))
-DO_event_mv <- data.frame(WellID,Eventdates,DO_event_mean,DO_event_cv)
-View(DO_event_mv)
-
-write_csv(DO_event_mv,"DTW_compiled/DO_mv_2days.csv")
-
-
+write_csv(DO_event_mv, "DTW_compiled/DO_mv_2days.csv")
 
 #### Temp whole Well Mean/Var ####
 wells<-c("SLOC","SLOW","VDOS","VDOW")
